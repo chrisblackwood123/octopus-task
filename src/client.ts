2d8e16c99b0bd21017b0127bbea4ec3d841d7a15
chris.blackwood <chris.blackwood20@gmail.com>
2025-11-25 20:52:53 +0000

    feat/get-outages: Implemented inital get outages functionality, which gets all outages from the kraken api using the supplied api key

---
+++ src/client.ts
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+export async function getOutages() {
+    try {
+        const response = await axios.get(`${process.env.BASE_URL}/outages`, {
+            headers: { "x-api-key": process.env.API_KEY}
+        });
+        return response.data;
+    } catch (err: any) {
+        const status = err?.response?.status;
+        console.error(status);
+        throw err;
+    }
+}
