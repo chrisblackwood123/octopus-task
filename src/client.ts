f4dfd76e022bcb072a1cf79075c16a26f4e5a7cd
chris.blackwood <chris.blackwood20@gmail.com>
2025-11-26 18:37:39 +0000

    test/send-outages: Added functionality to send an Outage object to the Kraken API

--- src/client.ts
+++ src/client.ts
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios, {type AxiosResponse} from "axios";
 import type {Outage, SiteInfo} from "./models.js";
 import { API_KEY, BASE_URL } from "./config.js";
 
@@ -10,7 +10,7 @@ export async function getOutages(): Promise<Outage[]> {
         return response.data;
     } catch (err: any) {
         const status = err?.response?.status;
-        console.error(status);
+        console.error("Failed to get outages:", status);
         throw err;
     }
 }
@@ -23,7 +23,23 @@ export async function getSiteInfo(siteId: string): Promise<SiteInfo> {
         return response.data;
     } catch (err: any) {
         const status = err?.response?.status;
-        console.error(status);
+        console.error("Failed to get site info: ", status);
+        throw err;
+    }
+}
+
+export async function sendOutages(outages: Outage[], siteId: string): Promise<AxiosResponse<any>> {
+    try {
+        return await axios.post(
+            `${BASE_URL}/site-outages/${siteId}`,
+            outages,
+            {
+                headers: { "x-api-key": API_KEY }
+            }
+        );
+    } catch (err: any) {
+        const status = err?.response?.status;
+        console.error("Failed to send outages", status);
         throw err;
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { OUTAGE_DATE, SITE_ID } from "./config.js";
 import 'dotenv/config'
 import {filterOutages} from "./filter.js";
-import {getOutages, getSiteInfo} from "./client.js";
+import {getOutages, getSiteInfo, sendOutages} from "./client.js";
 import * as console from "node:console";
 import {processOutages} from "./service.js";
 
@@ -9,6 +9,16 @@ async function main() {
     const siteInfo = await getSiteInfo(SITE_ID);
     const outages = await getOutages();
     const filteredOutages = filterOutages(outages, OUTAGE_DATE, siteInfo);
+    const processedOutages = processOutages(filteredOutages, siteInfo);
+    const postResponse = await sendOutages(processedOutages, SITE_ID);
+
+    console.log("Processed outages: ", processedOutages);
+
+    if (postResponse.status === 200) {
+        console.log("Successfully sent processed outages to the Kraken endpoint");
+    } else {
+        console.log("Error in sending processed outages to the Kraken endpoint");
+    }
 }
 
 main();
